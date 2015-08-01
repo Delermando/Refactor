ceb7c9d96389fcad005771354023a1f49a514bb1
delermando <delsantos@hotmail.com.br>
2015-08-01 02:10:52 -0300

    retornando indice da selecao

--- DevHelper.py
+++ DevHelper.py
@@ -1,7 +1,14 @@
 import sublime, sublime_plugin, pprint
 
 class DevHelper(sublime_plugin.TextCommand, sublime.View):
-	def run(self, edit):
-		#self.view.insert(edit, 0, "Alo")
-		self.view.set_name("Dev Helper! Hello Word")
-		print(str(self.view.name()))
+    def run(self, edit):
+        #self.view.insert(edit, 0, "Alo")
+        #self.view.set_name("Dev Helper! Hello Word")
+        #print(str(self.view.name()))
+
+        braces = False
+        sels = self.view.sel()
+        for sel in sels:
+            print(sel)
+            if self.view.substr(sel).find('{') != -1:
+                braces = True

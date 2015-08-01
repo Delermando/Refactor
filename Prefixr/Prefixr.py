85c2477528db79ab1163fd2e3ccd739ab1b022e6
Diogo Alves <diogoalvessilva86@gmail.com>
2015-08-01 01:11:29 -0300

    Estudando a API do sublime plugin

--- Prefixr/Prefixr.py
+++ Prefixr/Prefixr.py
@@ -1,5 +1,7 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, pprint
 
-class ExampleCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+class DeadCodeCommand(sublime_plugin.TextCommand, sublime.View):
+	def run(self, edit):
+		#self.view.insert(edit, 0, "Alo")
+		self.view.set_name("Diogo Alves")
+		print(str(self.view.name()))

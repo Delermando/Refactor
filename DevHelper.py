7dc7fbd7a1b5556cfe4d1c041e5bf0d1480e96b3
Delermando <delsantos@hotmail.com.br>
2015-08-21 00:26:36 -0300

    aplicando logica para ignorar uma funcao se ela tiver o mesmo nome da classe (construtor), e refatorando funcoes

--- DevHelper.py
+++ DevHelper.py
@@ -2,11 +2,10 @@ import sublime, sublime_plugin, pprint
 import re
 
 class DevHelper(sublime_plugin.TextCommand):
-    functionPattern = 'function'
-    visibilityPattern = 'private'
-    pointerPattern = '->'
-    functionRegex = visibilityPattern + '[\s\n]+' +functionPattern+'[\s\n]+(\S+)[\s\n]*\('
-    variablesRegex = visibilityPattern + '[\s\n]+\$+(\S+)'
+    pointer = '->'
+    classRegex = '(\s)*class(\s)+(\S+)((\s*)extends|\s)+(\S+)(\s)*{'
+    functionRegex = 'private[\s\n]+function[\s\n]+(\S+)[\s\n]*\('
+    variablesRegex = 'private[\s\n]+\$+(\S+)'
 
     def run(self, edit):
         privateObjects = self.getNotUsedObjectsFromContent(
@@ -15,12 +14,12 @@ class DevHelper(sublime_plugin.TextCommand):
         )
         self.draw(privateObjects.items())
 
-    def draw(self, objects):
-        print('-----------------not used--------------------------')
+    def draw(self, objects):        
+        print('-----------------Object Not Used--------------------------')
         for key, value in objects:    
-            print(key)
+            print(self.pointer + key)
             self.markLine(value)            
-        print('---------------------------------------------------')
+        print('----------------------------------------------------------')
 
     def getLinesCoordinates(self):
         return list(self.view.lines(sublime.Region(0,self.view.size())))
@@ -34,34 +33,63 @@ class DevHelper(sublime_plugin.TextCommand):
     def match(self, regex, content):
         return re.search( regex, content )
 
-    def findObjectOcorrences(self, coordinates, regex):
+    def findFunctionsOcorrences(self, coordinates, regex):
         result = {}
         for coordinate in coordinates:
             match = self.match(regex, self.getContentByRegion(coordinate))
             if match:
-                result[self.sanitize(match.group())] = coordinate
+                result[self.sanitizeFunction(match.group())] = coordinate
+        return result 
+
+    def findVariableOcorrences(self, coordinates, regex):
+        result = {}
+        for coordinate in coordinates:
+            match = self.match(regex, self.getContentByRegion(coordinate))
+            if match:
+                result[self.sanitizeVariable(match.group())] = coordinate
         return result
 
+    def getClassName(self,coordinates):
+        result = {}
+        match = self.match(self.classRegex, self.getContentByRegion(sublime.Region(0,self.view.size())))
+        if match:
+            result = self.sanitizeClass( match.group())
+        return result
+
+        
     def getFunctionNames(self):
-        return self.findObjectOcorrences(self.getLinesCoordinates(), self.functionRegex)    
+        functions = self.findFunctionsOcorrences(self.getLinesCoordinates(), self.functionRegex)    
+        className = self.getClassName(self.getLinesCoordinates())
+        if className in functions:
+            del functions[className]
+
+        return functions
 
     def getVariablesNames(self):
-        return self.findObjectOcorrences(self.getLinesCoordinates(), self.variablesRegex)    
+        return self.findVariableOcorrences(self.getLinesCoordinates(), self.variablesRegex)    
 
-    def sanitize(self, string):
-        remove = ['(', ' ', ';', '$', self.visibilityPattern, self.functionPattern]
-        for item in remove:
-            string = string.replace(item, '')
-        return string
+    def sanitizeClass(self, string):
+        return string.split()[1]
+
+    def sanitizeFunction(self, string):
+        return string.split()[2].replace('(','')
+
+    def sanitizeVariable(self, string):
+        string =  string.split()[1].replace('$','')
+        return string.replace(';','')
 
     def getPrivateObjects(self):
         return  dict(list(self.getVariablesNames().items()) + list(self.getFunctionNames().items()))
 
+
     def getNotUsedObjectsFromContent( self, content, objects ):
         frequency = {}
         for key, value in objects.items():
-            match =  re.findall( self.pointerPattern + key, content )
+            match =  re.findall( self.pointer + key, content )
             if len(match) == 0:
                 frequency[key] = value
 
-        return frequency    
+        return frequency   
+
+    
+
